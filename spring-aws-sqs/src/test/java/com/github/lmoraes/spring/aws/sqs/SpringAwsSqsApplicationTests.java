8fba3b508ae6d8fcb68d1bfdc436cf530a26a9f2
LMoraes7 <lukaocavalcante93@gmail.com>
2022-03-28 21:56:29 -0300

    subindo projeto

---
+++ spring-aws-sqs/src/test/java/com/github/lmoraes/spring/aws/sqs/SpringAwsSqsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.lmoraes.spring.aws.sqs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAwsSqsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
